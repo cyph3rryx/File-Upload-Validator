c382bc166d0f30177252dd78a4aa6c8182364302
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-03-03 22:46:33 +0530

    Update source.php
    The updated code includes the following advancements:
    
        File Type Validation: This checks the file type using the fileinfo extension, which detects the actual file type by examining the file's contents. It compares the MIME type to an array of allowed MIME types.
    
        Virus Scan: This performs a virus scan on the uploaded file using the ClamAV antivirus software. It checks if the scan result includes the "OK" message, which indicates that the file is free of malware.
    
        File Hashing: This generates a hash value for the uploaded file using the SHA256 algorithm. The hash value can be used to verify the integrity of the file later.
    
        Size Limit: This checks if the uploaded file size exceeds the maximum allowed size, which is set to 2 MB.
    
        File Naming Convention: This generates a unique name for the uploaded file based on its hash value and original name.
    
        Logging: This logs each successful file upload to a file named "upload.log" located in the "logs" directory. The log message includes the file name, user IP address, and upload time.
    
    Note that the virus scan implementation in this code requires ClamAV to be installed on the server. You can adjust this implementation to use other antivirus software or online scanning services if needed.

--- source.php
+++ source.php
@@ -4,20 +4,39 @@ if(isset($_POST["submit"])){
     $file_size = $_FILES['file']['size'];
     $file_tmp = $_FILES['file']['tmp_name'];
     $file_type = $_FILES['file']['type'];
-    $file_ext = strtolower(end(explode('.',$_FILES['file']['name'])));
+    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
     $extensions = array("jpeg","jpg","png","pdf","doc","docx","xls","xlsx");
 
-    if(in_array($file_ext,$extensions)=== false){
-         echo "Extension not allowed, please choose a JPEG, JPG, PNG, PDF, DOC, DOCX, XLS, or XLSX file.";
-    }else{
-         if($file_size > 2097152){
-            echo 'File size must be less than 2 MB';
-         }else{
-            $target_dir = "uploads/";
-            $target_file = $target_dir . basename($_FILES["file"]["name"]);
-            move_uploaded_file($file_tmp, $target_file);
-            echo "File uploaded successfully.";
-         }
+    // File Type Validation
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $mime_type = finfo_file($finfo, $file_tmp);
+    if (!in_array($mime_type, $mime_types)) {
+        echo "Invalid file type. Please upload a valid file.";
+    } else {
+        // Virus Scan
+        $virus_scan = shell_exec("clamscan - -no-summary - -detect-pua $file_tmp");
+        if (strpos($virus_scan, "OK") !== false) {
+            // File Hashing
+            $file_hash = hash_file("sha256", $file_tmp);
+            // Size Limit
+            $max_size = 2097152;
+            if($file_size > $max_size){
+                echo 'File size must be less than 2 MB';
+            } else {
+                // File Naming Convention
+                $target_dir = "uploads/";
+                $target_file = $target_dir . $file_hash . "_" . basename($file_name);
+                move_uploaded_file($file_tmp, $target_file);
+                // Logging
+                $upload_time = date("Y-m-d H:i:s");
+                $user_ip = $_SERVER['REMOTE_ADDR'];
+                $log_message = "$file_name uploaded successfully by $user_ip at $upload_time";
+                error_log($log_message, 3, "logs/upload.log");
+                echo "File uploaded successfully.";
+            }
+        } else {
+            echo "The file you are attempting to upload appears to be infected with a virus or malware. Please scan your device for viruses and try again.";
+        }
     }
 }
 ?>
